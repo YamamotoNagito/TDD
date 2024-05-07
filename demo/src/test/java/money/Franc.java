2f8e71db0476ef0ae51e421f907f8de04a969dd5
nnaa <nnaa99toooin@example.com>
2024-05-08 08:25:18 +0900

    green:francを追加

---
+++ demo/src/test/java/money/Franc.java
@@ -0,0 +1,16 @@
+package money;
+
+class Franc{
+    private int amount;
+    Franc(int amount){
+        this.amount = amount;
+    }
+    Franc times(int multiplier){
+        // amount *= multiplier;
+        return new Franc(amount * multiplier);
+    }
+    public boolean equals(Object object){
+        Franc Franc = (Franc) object;
+        return amount == Franc.amount;
+    }
+}
