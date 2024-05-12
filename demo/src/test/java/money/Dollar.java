1f6bbe745306b8decc51ece327bd1f7088c66afd
YamamotoNagito <nnaa99toooin@gmail.com>
2024-05-12 14:12:36 +0900

    green

--- demo/src/test/java/money/Dollar.java
+++ demo/src/test/java/money/Dollar.java
@@ -4,6 +4,11 @@ class Dollar extends Money{
     Dollar(int amount){
         this.amount = amount;
     }
+
+    String currency(){
+        return "USD";
+    }
+
     Money times(int multiplier){
         // amount *= multiplier;
         return new Dollar(amount * multiplier);
--- demo/src/test/java/money/Franc.java
+++ demo/src/test/java/money/Franc.java
@@ -4,6 +4,11 @@ class Franc extends Money{
     Franc(int amount){
         this.amount = amount;
     }
+
+    String currency(){
+        return "CHF";
+    }
+
     Money times(int multiplier){
         // amount *= multiplier;
         return new Franc(amount * multiplier);
